908ef9b003b4f641d63db53cb707edd59e948b64
shihyuhuang <61773397+shihyuhuang@users.noreply.github.com>
2021-01-22 15:35:38 +0800

    Add files via upload

---
+++ exam03-master/another.py
@@ -0,0 +1,41 @@
+import paho.mqtt.client as paho
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+# https://os.mbed.com/teams/mqtt/wiki/Using-MQTT#python-client
+
+# MQTT broker hosted on local machine
+mqttc = paho.Client()
+
+# Settings for connection
+# TODO: revise host to your ip
+host = "170.134.23.10"
+topic = "velocity"
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+      print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+      data = str(msg.payload)
+      print("[Received] Topic: " + msg.topic + ", Message: " + data)
+
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+      print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+      print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+mqttc.loop_forever()
---
+++ exam03-master/client.py
@@ -0,0 +1,93 @@
+import paho.mqtt.client as paho
+import serial
+import time
+import numpy as np
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x140\r\n".encode())
+char = s.read(3)
+print("Set MY 0x140.")
+print(char.decode())
+
+s.write("ATDL 0x240\r\n".encode())
+char = s.read(3)
+print("Set DL 0x240.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+print("start sending RPC")
+
+
+
+AA = np.ones(10)
+s.write("/GetA/run\r".encode())
+for i in range(10):
+    velo = float(s.readline())
+    print(velo)
+    AA.append(velo)
+
+mqttc = paho.Client()
+host = "170.134.23.10"
+topic = "velocity"
+def on_connect(self, mosq, obj, rc):
+      print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+      print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+      print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+      print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
+# Publish messages from Python
+jj = 0
+while jj != 10:
+      ret = mqttc.publish(topic, str(AA[num]) + "\n", qos=0)
+      if (ret[0] != 0):
+            print("Publish failed")
+      mqttc.loop()
+      time.sleep(0.5)
+      jj =jj+ 1
+s.close()

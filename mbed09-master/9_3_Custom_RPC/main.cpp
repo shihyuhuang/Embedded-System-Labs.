0c3e994a8ce70b273dc275e92122f9d2c1668589
shihyuhuang <61773397+shihyuhuang@users.noreply.github.com>
2021-01-22 15:21:49 +0800

    Add files via upload

---
+++ mbed09-master/9_3_Custom_RPC/main.cpp
@@ -0,0 +1,151 @@
+#include "mbed.h"
+
+#include "mbed_rpc.h"
+
+
+/**
+
+ *  This example program has been updated to use the RPC implementation in the new mbed libraries.
+
+ *  This example demonstrates using RPC over serial
+
+**/
+
+RpcDigitalOut myled1(LED1,"myled1");
+
+RpcDigitalOut myled2(LED2,"myled2");
+
+RpcDigitalOut myled3(LED3,"myled3");
+
+Serial pc(USBTX, USBRX);
+
+void ll(Arguments *in, Reply *out);
+
+RPCFunction rpcLED(&ll, "ll");
+
+double x, y;
+
+
+int main() {
+
+    //The mbed RPC classes are now wrapped to create an RPC enabled version - see RpcClasses.h so don't add to base class
+
+
+    // receive commands, and send back the responses
+
+    char buf[256], outbuf[256];
+
+    while(1) {
+
+        memset(buf, 0, 256);
+
+        for (int i = 0; ; i++) {
+
+            char recv = pc.getc();
+
+            if (recv == '\r') {
+
+                pc.printf("\r\n");
+
+                break;
+
+            }
+
+
+            buf[i] = pc.putc(recv);
+
+        }
+        //Call the static call method on the RPC class
+
+        RPC::call(buf, outbuf);
+
+        pc.printf("%s\r\n", outbuf);
+
+    }
+
+}
+
+
+// Make sure the method takes in Arguments and Reply objects.
+
+void ll (Arguments *in, Reply *out)   {
+
+    bool success = true;
+
+
+    // In this scenario, when using RPC delimit the two arguments with a space.
+
+    x = in->getArg<double>();
+
+    y = in->getArg<double>();
+
+
+    // Have code here to call another RPC function to wake up specific led or close it.
+
+    char buffer[200], outbuf[256];
+
+    char strings[20];
+
+    int led = x;
+    int on=y;
+
+    int k=0;
+    while(1){
+    led=1;
+    on=1;
+
+
+    if(k>=7){
+        k=3;
+    }
+    if(k==0){
+        led=1;
+        on=1;
+    }
+    else if(k==1){
+        led=2;
+        on=1;
+    }
+    else if(k==2){
+        led=3;
+        on=1;
+    }
+    else if(k==3){
+        led=1;
+        on=0;
+        wait(0.5);
+    }
+    else if(k==4){
+        led=1;
+        on=1;
+        wait(0.5);
+    }
+    else if(k==5){
+        led=3;
+        on=0;
+        wait(0.5);
+    }else{
+        led=3;
+        on=1;
+        wait(0.5);
+    }
+
+    int n = sprintf(strings, "/myled%d/write %d", led, on);
+
+    strcpy(buffer, strings);
+
+    RPC::call(buffer, outbuf);
+
+    if (success) {
+
+        out->putData(buffer);
+
+    } else {
+
+        out->putData("Failed to execute LED control.");
+
+    }
+    k=k+1;
+    }
+    
+}
